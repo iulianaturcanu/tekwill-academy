ca21e7037ae7a5be3bac7e8bc8fea994f32d0524
iulianaturcanu <iuliana110@gmail.com>
2023-06-02 20:53:24 +0300

    Add materials for lesson #16

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,183 +1,98 @@
-// const now = 2023;
-// console.log(now - 1990 > now - 2016);
+// // Functions
 
-// 1. now - 1990 // 33;
-// 2. now - 2016 // 7;
-// 3. 33 > 7 // true;
+// // Declararea functiei
+// // function logger() {
+// //     // function body
+// //     // console.log("welcome to our course");
+// // }
 
-// Operator Precendence - cu cat e mai mare valoare, 
-// cu atat e mai importanta opertiunea
+// // // Call | run | invoke function
 
-// let z , y;
+// // logger();
 
-// assignment right to left
-// z = y = 25 - 10 - 5;
-// z = y = 10;
-// console.log(z, y); // 10 10
+// // // Functia poate primi parametri(optional) si sa returneze valori inapoi
 
-const result = 2 + 3 * 4; // 14
+// // function fruitProcessor(kiwi, banana) {
+// //     // console.log(kiwi, banana);
+// //     const juice = `Juice with ${kiwi} kiwi and ${banana} bananas`;
+// //     return juice;
+// // }
 
-// left-to-right
-// 25 - 10 - 5;
-//10;
+// // fruitProcessor(5, 3);
 
-// const ageA = 30;
-// const ageB = 40;
-// const averageAge = (ageA + ageB) / 2;
-// 30 + 40 / 2 = 30 + 20 = 50
-// 70 / 2 = 35
+// // // `Juice with ${kiwi} kiwi and ${banana} bananas`
+// // const result = fruitProcessor(1,5); 
+// // console.log(result);
 
-// If/else
+// // function calculateAge(birthYear) {
+// //     const date = new Date();
+// //     const year = date.getFullYear();
+// //     const age = year - birthYear;
+// //     return console.log(`My age is: ${age}`);
+// // }
 
-// Program pentru verificarea varstei de vot
+// // calculateAge(1990);
 
-// const age = 5;
-const controlAge = 18;
-// const isOldEnough = age >= controlAge;
+// // calculatAge2(1800);
 
-// In parantezele rotunde se pune conditia
-// conditia este o expresie care returneaza true sau false
-if (isOldEnough) {
-    console.log('User can vote');
-} else {
-    const yearsLeft = controlAge - age;
-    console.log(`User cannot vote yet. Wait ${yearsLeft} years.`);
+// // Functii cu o linie de cod inauntru
+// // Aici se depisteaza o eroare
+// // const calculatAge3 = birthYear => {
+// //     console.log(birthYear); 
+// //     return `My age is: ${2023 - birthYear}`;
+// // }
+
+// // calculatAge3(1700);
+
+// // const calculatAge4 = (birthYear) => {
+// //     return console.log(`My age is: ${2023 - birthYear}`);
+// // };
+
+// // calculatAge4(1600);
+
+// // const sum = (value1, value2) => value1 + value2;
+
+// // hardcode - a-i forta o valoare fixa 
+
+// // Arrow function
+// const calculatAge2 = (birthYear) => {
+//     const date = new Date();
+//     const year = date.getFullYear();
+//     const age = year - birthYear;
+//     return age;
+// };
+
+// const showMessage = (age, retirementAge, firstName) => {
+// if (age < 67 ) {
+//         return `${firstName} retires in ${retirementAge - age} years`;
+//     } else {
+//         return `${firstName} already retired ${age - retirementAge} years ago`;
+//     }
+// }
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = calculatAge2(birthYear);
+//     // hardcode retirement age - it should be comming from backend
+//     return showMessage(age, 67, firstName);
+// }
+
+// console.log(yearsUntilRetirement(1900, "Iuliana"));
+
+// Scrieți o funcție numită tellFortune care:
+// are 4 argumente: numărul de copii, numele partenerului, locația geografică, titlul pozitiei.
+// vă arată averea pe ecran astfel: "You will be a X in Y, and married to Z with N kids.".
+// Apelați acea funcție de 3 ori cu 3 valori diferite pentru argumente.
+
+const tellFortune = (children, partner, location, jobTitle) => {
+    if (partner > 20 && children > 20) {
+        return `You will be a ${jobTitle} in ${location}, and married to ${partner} with ${children} kids`;
+    } else if (partner && !children) {
+        return `You will be a ${jobTitle} in ${location}, and married to ${partner}`;
+    } else {
+        return `You will be a ${jobTitle} in ${location}`;
+    }
 }
 
-// conditia else este optionala
-// if (isOldEnough) {
-//     console.log('User can vote');
-// }
-
-// const birthYear = 1990; // secolul 20
-// let century; // undefined
-
-// if (birthYear >= 2000) {
-//     console.log('We entered if statement');
-//     century = 20;
-// } else {
-//     console.log('We entered else statement');
-//     century = 21;
-// }
-
-// console.log(`Century is ${century}`);
-
-
-// Type conversion and coercion
-
-
-const year = Number(inputYear);
-const eligibleAge = 2023 - year;
-
-const minutes = 23;
-console.log(String(minutes));
-
-
-// Type coercion
-
-// Adunarea la string si number se returneaza string (concatenare)
-console.log(inputYear + 18); // '1991' + '18' = 199118
-const inputYear = '1991';
-// Scaderea, inmultirea si impartirea la string si number se returneaza number
-console.log(inputYear - 18); // 1973
-
-// NaN - not a number
-// console.log(typeof NaN);
-
-console.log(Number("1991")); // rezultat: 1991
-console.log(Number("1991abc")); // rezultat: NaN
-console.log(Number("abc")); // rezultat: NaN
-
-
-// examples type coercion
-
-const message = 'I am ' + 23 + ' years old';
-console.log(typeof message); // I am 23 years old
-
-const data = '23' + 'Iuliana' - 3; // NaN
-console.log(typeof data); // 23 - 10 - 3 = 10
-
-console.log(2 + 3 + '5'); // '55' string
-// 1. 2 + 3 = 5
-// 2. 5 + '5' = '55'
-
-console.log('10' - '4' - 2 + '5'); // '45' string
-// 1. 10 - 4 = 6 number
-// 2. 6 - 2 = 4 number
-// 3. 4 + '5' = '45' string
-
-// Putem schimba type la 3 tipuri de date: string, number, boolean
-
-// Truthy and Falsy values
-// falsy - valori care nu tocmai sunt false, dar daca sunt convertite in boolean va da false
-
-// 5 falsy values: 0, '', undefined, null, NaN
-
-console.log('boolean:', Boolean(100)); // false
-
-// 0 - false
-// 1 - true
-
-let cars;
-
-let age = 16;
-let hasParentConsent = true;
-
-if (age >= 18 || hasParentConsent) {
-  console.log("Persoana este eligibilă să participe la eveniment");
-}
-
-
-
-// if (height) {
-//     // if (height > 5) {
-//     //     console.log('Height is bigger than 5');
-//     // } else {
-//     //     console.log('Height is defined, but is smaller than 5');
-//     // }
-// } else if () {
-//     console.log('Height is undefined');
-// } 
-
-// == & ===
-// === - comparatie stricta (verifica valoarea si tipul de date)
-// == - loose comparation (verifica doar valoarea)
-
-// const userAgeInput = '18';
-// if (Number(userAgeInput) === 18) console.log('You just became an adult');
-
-// Boolean logic - AND (&&), OR ( || ), NOT ( ! )
-
-// conditia A - Ana has 18 years;
-// conditia B - Ana has driving license;
-
-// Construim un formular de angajare
-// if (conditia A && conditia B) {
-    // A - true si B - true 
-    // pozitia de sofer
-// } else {
-    // A - false si B - false
-    // A - true si B - false
-    // A - false si B - true
-    // pozitia de vanzator
-// }
-
-// if (!conditia A) console.log('Ana can not be employed');
-
-// if (conditia A) {
-//     console.log('Ana can be employed');
-// } else {
-//     console.log('Ana can not be employed');
-// }
-
-const laptops = 10;
-
-// A: Noi avem mai mult de 20 de laptopuri; false 10 > 20
-// B: Noi avem mai putin sau egal cu 40 de laptopuri; true 10 <= 40;
-
-// !A - true
-// !B - false
-// A && B - false
-// A || B - true 10 > 20 sau 10 <= 40
-// !A && !B  - true && false = false
-// A || B || C
+console.log(tellFortune(10, 'Bob', 'Australia', 'doctor'));
+console.log(tellFortune( 10, undefined, 'Australia', 'doctor'));
+console.log(tellFortune( undefined, 'Tom', 'Australia', 'doctor'));
