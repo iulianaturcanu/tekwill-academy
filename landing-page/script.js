c4baa12530eba0b33ec4effc88226f0c74313ab0
iulianaturcanu <iuliana110@gmail.com>
2023-06-21 18:39:09 +0300

    Add materials for lesson #23

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,180 +1,87 @@
-// // 1. Sintaxa
-// // function sum(a, b) {
-// //   return a+b;
-// // }
+// // Accesing Elements
+// const element = document.getElementById('myElement');
+// // const image = document.getElementsByClassName('image');
+// const image = document.querySelector('.image');
+// const [button] = document.getElementsByClassName('image-button');
 
-// // const sum = function(a, b) {
-// //   return a+b;
-// // }
+// // Modifying Elements
 
-// // const sum = (a, b) => {
-// //   return a + b;
-// // }
+// const changeBackground = () => {
+//   element.style.backgroundColor = '#e45f34';
+//   // element.textContent = 'New content';
+//   element.innerHTML = `<form class="form-container">
+//                 <input type="text" placeholder="Full Name">
+//                 <input type="email" placeholder="example@gmail.com">
+//                 <select>
+//                     <option value="option1">Option 1</option>
+//                     <option value="option2">Option 2</option>
+//                 </select>
+//                 <select>
+//                     <option value="option3">Option 3</option>
+//                     <option value="option4">Option 4</option>
+//                 </select>
+//                 <input type="text" placeholder="Message">
+//             </form>`;
+//   button.removeAttribute('disabled');
+// }
 
-// // 2. Contextul 'this'
-// const person = {
-//   name: 'Tom',
-//   greet: function() {
-//     console.log(this);
-//     return console.log('Salut, ma numesc ' + this.name);
+
+// const showImage = () => {
+//   // console.log(image.style.display);
+//   if (image.style.display === 'none') {
+//     image.style.display = 'block';
+//     button.textContent = 'Hide image';
+//   } else {
+//     image.style.display = 'none';
+//     button.textContent = 'Show image';
 //   }
 // }
 
-// // console.log(person.greet());
+// button.addEventListener('click', showImage);
 
-// //Gresit 
-// // const person2 = {
-// //   name: 'Ana',
-// //   greet: () => {
-// //     console.log(this);
-// //     console.log('Salut, ma numesc ' + this.name);
-// //   }
-// // }
+// const form = document.querySelector('.form-container');
 
-// // const person2 = {
-// //   name: 'Ana',
-// //   greet: (personName) => {
-// //     // const name = 'Ana';
-// //     console.log('Salut, ma numesc ' + personName);
-// //   }
-// // }
+// form.addEventListener('submit', (event) => {
+//   // To prevent form submission
+//   event.preventDefault();
+//   const fullNameInput = document.querySelector('.fullName');
+//   const emailInput = document.querySelector('input[type="email"]');
+//   const select1 = document.querySelector('select:nth-of-type(1)');
+//   const select2 = document.querySelector('select:nth-of-type(2)');
+//   const messageInput = document.querySelector('.message-input');
 
-// // person2.greet(person2.name);
+//   if (fullNameInput.value.trim() === '') {
+//     alert('Please enter your name');
+//     emailInput.focus();
+//   }
 
-// // // 3. Constructor objects
-// // function Person(name, age) {
-// //   this.name = name;
-// //   this.age = age;
-// // }
+//   console.log(fullNameInput.value.trim());
+//   console.log(emailInput.value);
+//   console.log(select1.value);
+//   console.log(select2.value);
+//   console.log(messageInput.value);
+  
+//   form.reset();
+// });
 
-// // const american = new Person('Tom', 30);
 
-// // const Person2 = (name, age) => {
-// //   this.name = name;
-// //   this.age = age;
-// // }
+// const fetchData = async () => {
+// const url = 'https://restcountries.com/v3.1/all';
+// const options = {
+// 	method: 'GET',
+// };
 
-// // const american2 = new Person2('Ana', 40);
-
-// // 4. metoda arguments
-// function sum2() {
-//   // console.log(arguments.length)
-//   // let total = 0;
-//   // for (let i = 0; i < arguments.length; i++) {
-//   //   total += arguments[i];
-//   // }
-//   // arguments - array-like object
-//   const args = [...arguments];
-//   const total = args.reduce((total, number) => total + number);
-//   return total;
-//   // return arguments.reduce((total, number) => total + number);
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     console.log(result);
+//     if(result) {
+//     document.getElementById("image").innerHTML = "<img src='" + result[0].flags.png + "' alt='Movie Poster' />";
+//     // document.getElementById("release-year").textContent = "Release Year: " + movie.year;
+//     //     document.getElementById("title").textContent = "Title: " + movie.title;
+//   }} catch (error) {
+//     console.error(error);
+//   }
 // }
 
-// const sum = (...numbers) => {
-//   // ...numbers egal cu [numbers]
-//   return numbers.reduce((total, number) => total + number);
-// }
-// console.log(sum2(1, 2, 3, 4, 5)); // []
-
-// Problemă 2: Găsește cel mai lung cuvânt într-un string
-// Scrie o funcție care primește un string și returnează cel mai lung cuvânt din acel string.
-
-const findTheLongestWord = (str) => {
-  let theLongestWord = ''; // 100% string
-  const words = str.split(' '); // ["Today", ...]
-  for (const word of words) {
-    // word: ''
-    console.log({word});
-    // console.log('word: ' + word);
-    if (word.length > theLongestWord.length) {
-      theLongestWord = word;
-    }
-  }
-
-  return theLongestWord;
-}
-
-console.log(findTheLongestWord('Today is a beautiful day'));
-
-const path = '127.0.0.1:5501/landing-page/index.html';
-const paths = path.split('/');
-
-// Gasirea celui mai mare numar dintr-un array
-const findTheBiggestNumber = (array) => {
-  // Math.max(1, 2, 3, 4, 5)
-  // return Math.max(...array);
-  // Presupunem ca primul element din array este cel mai mare
-  let theBiggestNumber = array[0];
-
-  for (let i = 1; i < array.length; i++) {
-    if(array[i] > theBiggestNumber) {
-      theBiggestNumber = array[i];
-    }
-  }
-
-  return theBiggestNumber;
-}
-
-// Concatinarea la 2 array-uri
-
-const concatinateArrays = (array1, array2) => {
-  // [1, 2, 3, 4, 5, 6]
-  return array1.push(...array2);
-  // const [...rest] = array1;
-  // const [...rest2] = array2;
-  // [rest, rest2]
-  // return [...array1, ...array2];
-}
-
-// const array1 = [1, 2, 3, 4];
-// const array2 = [5, 6];
-
-// Problemă 5: Calcularea sumei și diferenței a două numere
-// Scrie o funcție care primește un obiect destructurat cu proprietățile a și b
-// și returnează un alt obiect care conține suma și diferența acestor două numere.
-
-// // const person2 = {
-// //   name: 'Ana',
-// //   greet: (personName) => {
-// //     // const name = 'Ana';
-// //     return console.log('Salut, ma numesc ' + personName);
-// //   }
-// // }
-
-// // person2.greet();
-
-const calculateSumAndDiff = (input) => {
-  const {a, b} = input;
-  return {
-    sum: a+b,
-    diff: a-b
-  }
-}
-const object = calculateSumAndDiff({a: 4, b: 14}) // output object cu 2 key: sum si diff
-const input = {a: 4, b: 14}
-console.log(calculateSumAndDiff(input));
-
-// Problemă 2: Inversarea literei mari cu cea mică
-// Scrie o funcție care primește un string și 
-// inversează literele mari cu cele mici și viceversa.
-
-const inverseLetters = (str) => {
-  let inversedStr = '';
-
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    // H, H este egal cu H (.toUpperCase())
-    if(char === char.toUpperCase()) {
-      //1. H
-      //2. e
-      //n !
-      inversedStr += char.toLowerCase();
-    } else {
-      inversedStr += char.toUpperCase();
-    }
-  }
-
-  return inversedStr; // !
-}
-
-console.log(inverseLetters('Hello word!'));
+// fetchData();
